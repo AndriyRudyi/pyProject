38ba8ed4de6f2989197acf647704b270e07ddeff
AndriyRudyi <gamere4ek88@ukr.net>
2023-09-10 14:48:26 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from tiktok import TikTokApi
+
+# TG token fron Bot father
+TOKEN = '6427072934:AAHiuCanWVIFCVE1qrHNKpXb8VApyKfFRz4'
+api = TikTokApi()
+
+def start(update, context):
+    update.message.reply_text('Hi send me url ')
+
+def download_tiktok_video(update, context):
+    url = update.message.text
+    try:
+        video = api.get_video_by_url(url)
+        if video:
+            update.message.reply_video(video['url'])
+        else:
+            update.message.reply_text('video could not be found')
+    except Exception as e:
+        print(str(e))
+        update.message.reply_text('An error occurred while uploading the video.')
+
+def main():
+    updater = Updater(token=TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, download_tiktok_video))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
